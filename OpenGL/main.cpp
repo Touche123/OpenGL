5298f208b963821d2e06d0fc57ee580ae5c42552
Touche123 <jonaswestman@hotmail.com>
2014-11-03 02:07:52 +0100

    o

--- OpenGL/main.cpp
+++ OpenGL/main.cpp
@@ -18,8 +18,8 @@ void initShaders(sf::Shader& shader, std::string vert, std::string frag) {
 
 void display(sf::Window* window){
 	// clear the buffers
-	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-
+	
+	glDrawArrays(GL_TRIANGLES, 0, 3);
 	// draw...
 	window->display();
 	// end the current frame (internally swaps the front and back buffers)
@@ -29,13 +29,15 @@ int main()
 	// create the window
 	sf::Window window(sf::VideoMode(1600, 900), "OpenGL", sf::Style::Default, sf::ContextSettings(32));
 	window.setVerticalSyncEnabled(true);
-	
+
+	float dt = 0.0f;
+
 	// load resources, initialize the OpenGL states, ...
 	sf::Shader shader;
 	initShaders(shader, "assets/shaders/shader.vert", "assets/shaders/shader.frag");
-
+	
 	shader.bind(&shader);
-
+	
 	GLenum err = glewInit();
 	if (GLEW_OK != err)
 	{
@@ -44,30 +46,43 @@ int main()
 	}
 	glewExperimental = GL_TRUE;
 
-	GLfloat vertices[12] = {
-			1.0f, 0.0f,
-			1.0f, 1.0f,
-			0.0f, 1.0f,
-
-			1.0f, 0.1f,
-			1.0f, 0.5f,
-			0.0f, 0.5f,
+	GLfloat vertices[] = {
+			-1.0f, -1.0f, 0.0f, 
+			1.0f, -1.0f, 0.0f,
+			0.0f, 1.0f, 0.0f,
 	};
 
+	
+	GLuint vao= 0;
+	GLuint vbo = 0;
+	GLuint vPosition = 0;
+	glGenVertexArrays(1, &vao);
+	glBindVertexArray(vao);
 
-	GLuint VAOs[5];
-	GLuint VBOs[5];
-	glGenVertexArrays(5, VAOs);
-	glBindVertexArray(*VAOs);
-	glGenBuffers(5, VBOs);
-	glBindBuffer(GL_ARRAY_BUFFER, *VBOs);
+	glGenBuffers(1, &vbo);
+	glBindBuffer(GL_ARRAY_BUFFER, vbo);
+
 	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+	glEnableVertexAttribArray(0);
+	glBindBuffer(GL_ARRAY_BUFFER, vbo);
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, (void*)0);
+	
+	
+	/*shader.setParameter("test", 0.0f, 0.0f, 0.0f, 1.0f);
+	shader.setParameter("offset", 0.0, 0.0);*/
 	///test
 	// run the main loop
 	bool running = true;
-
+	sf::Transform transform;
+	
+	
 	while (running)
 	{
+		dt += 0.05f;
+		
+		//shader.setParameter("time", dt);
+		
 		// handle events
 		sf::Event event;
 		while (window.pollEvent(event))
@@ -77,6 +92,7 @@ int main()
 				// end the program
 				running = false;
 			}
+			
 			else if (event.type == sf::Event::Resized)
 			{
 				// adjust the viewport when the window is resized
@@ -93,14 +109,16 @@ int main()
 				}
 				
 			}
+			
 		}
-
+		
+		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 		
 		display(&window);
 		
 		
 	}
-	
+	glDisableVertexAttribArray(0);
 	// release resources...
 
 	return 0;
